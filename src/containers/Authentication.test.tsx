1855023e0233c12bd3bb755c66f2ec1065731bdd
shubham.Affinidi <shubham.b@affinidi.com>
2021-11-03 15:47:28 +0530

    test updated

--- src/containers/Authentication.test.tsx
+++ src/containers/Authentication.test.tsx
@@ -21,44 +21,38 @@ const TestComponent: FC = () => {
   }
 
   async function onSignOut() {
-      try {
-          await signOut();
-      } catch (err) {
-          alert(err.message);
-      }
+    try {
+      await signOut();
+    } catch (err) {
+      alert(err.message);
+    }
   }
 
   return (
     <div className='Login'>
       <div className='Form'>
         <Form style={{ width: 280 }}>
-            
-            <Button block disabled={loading} onClick={onSignOut}>Sign Out</Button>:
-            <Button block disabled={loading} onClick={onLogin}>Login</Button>
+
+          <Button block disabled={loading} onClick={onSignOut}>Sign Out</Button>:
+          <Button block disabled={loading} onClick={onLogin}>Login</Button>
 
         </Form>
       </div>
     </div>
-    
+
   );
 };
 
 // Mock modules
 jest.mock('../services/SdkService');
 jest.mock('../services/MessageService');
-jest.mock("@affinidi/wallet-browser-sdk", ()=>({
-    AffinidiWallet: jest.fn()
+jest.mock("@affinidi/wallet-browser-sdk", () => ({
+  AffinidiWallet: jest.fn()
 }));
-jest.mock("@affinidi/wallet-core-sdk", ()=>({
-    __dangerous: {}
+jest.mock("@affinidi/wallet-core-sdk", () => ({
+  __dangerous: {}
 }));
-jest.mock("@affinidi/affinidi-did-auth-lib/dist/DidAuthService/DidAuthService", ()=>{});
-
-
-jest.mock("@affinidi/common/dist/services/JwtService", ()=>({
-    // ...jest.requireActual('@affinidi/common/dist/services/JwtService'),
-    fromJWT: jest.fn()
-}));
+jest.mock("@affinidi/affinidi-did-auth-lib/dist/DidAuthService/DidAuthService", () => { });
 
 const mockAlert = jest.spyOn(window, 'alert');
 jest.spyOn(console, 'error');
@@ -68,71 +62,71 @@ const MockSdkService = SdkService as jest.Mocked<typeof SdkService>;
 const networkMember: Wallet = new Wallet('testPassword', 'testEncryptedSeed');
 const SDK_AUTHENTICATION_LOCAL_STORAGE_KEY = "affinidi:accessToken";
 
-describe('Test Authentication context', ()=>{
+describe('Test Authentication context', () => {
 
-    test('signOut', async ()=>{
-        const sdk = new MockSdkService(await networkMember) as any
-        sdk.accessToken = 'someAccessToken'
-        MockSdkService.fromLoginAndPassword.mockReturnValue(sdk)
-        sdk.signOut.mockImplementation()
+  test('signOut', async () => {
+    const sdk = new MockSdkService(await networkMember) as any
+    sdk.accessToken = 'someAccessToken'
+    MockSdkService.fromLoginAndPassword.mockReturnValue(sdk)
+    sdk.signOut.mockImplementation()
 
-        const { getByRole } = render(
-            <AuthenticationProvider>
-                <TestComponent />
-            </AuthenticationProvider>
-        )
-        const loginButton = getByRole('button', {name: 'Login'});
+    const { getByRole } = render(
+      <AuthenticationProvider>
+        <TestComponent />
+      </AuthenticationProvider>
+    )
+    const loginButton = getByRole('button', { name: 'Login' });
 
-        
 
-        await act(async ()=>{
-            await userEvent.click(loginButton)
-        })
 
-        const signOutButton = getByRole('button', {name: 'Sign Out'})
-
-        await act(async ()=>{
-            await userEvent.click(signOutButton)
-        })
-
-        await waitFor(()=>expect(sdk.signOut).toBeCalled());
-
-        expect(localStorage.getItem(SDK_AUTHENTICATION_LOCAL_STORAGE_KEY)).toBeNull();
+    await act(async () => {
+      await userEvent.click(loginButton)
     })
 
-    test('signOut error', async ()=>{
-        const sdk = new MockSdkService(await networkMember) as any
-        sdk.accessToken = 'someAccessToken'
-        MockSdkService.fromLoginAndPassword.mockReturnValue(sdk)
+    const signOutButton = getByRole('button', { name: 'Sign Out' })
 
-        const error = new Error("Some Error");
-        sdk.signOut.mockImplementation(()=>{
-            throw error;
-        })
-
-        const { getByRole } = render(
-            <AuthenticationProvider>
-                <TestComponent />
-            </AuthenticationProvider>
-        )
-
-        const loginButton = getByRole('button', {name: 'Login'});
-
-        await act(async ()=>{
-            await userEvent.click(loginButton)
-        })
-
-        const signOutButton = getByRole('button', {name: 'Sign Out'})
-
-        await act(async ()=>{
-            await userEvent.click(signOutButton)
-        })
-
-        await waitFor(()=>expect(sdk.signOut).toBeCalled());
-        expect(mockAlert).toBeCalledWith(error.message);
+    await act(async () => {
+      await userEvent.click(signOutButton)
     })
 
-    afterEach(()=>{
-        localStorage.clear();
+    await waitFor(() => expect(sdk.signOut).toBeCalled());
+
+    expect(localStorage.getItem(SDK_AUTHENTICATION_LOCAL_STORAGE_KEY)).toBeNull();
+  })
+
+  test('signOut error', async () => {
+    const sdk = new MockSdkService(await networkMember) as any
+    sdk.accessToken = 'someAccessToken'
+    MockSdkService.fromLoginAndPassword.mockReturnValue(sdk)
+
+    const error = new Error("Some Error");
+    sdk.signOut.mockImplementation(() => {
+      throw error;
     })
+
+    const { getByRole } = render(
+      <AuthenticationProvider>
+        <TestComponent />
+      </AuthenticationProvider>
+    )
+
+    const loginButton = getByRole('button', { name: 'Login' });
+
+    await act(async () => {
+      await userEvent.click(loginButton)
+    })
+
+    const signOutButton = getByRole('button', { name: 'Sign Out' })
+
+    await act(async () => {
+      await userEvent.click(signOutButton)
+    })
+
+    await waitFor(() => expect(sdk.signOut).toBeCalled());
+    expect(mockAlert).toBeCalledWith(error.message);
+  })
+
+  afterEach(() => {
+    localStorage.clear();
+  })
 })
